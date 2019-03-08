47e0cf3a050e81658349f2c9507ba48d6d51d342
Gortax <marcin.pilarczyk12@gmail.com>
2019-03-08 17:57:48 +0000

    Jump, Lava spills done
    Okay guys.... this is epic.

--- Console Game/Assets/Scenes/David Pacheco/Scripts/Movement.cs
+++ Console Game/Assets/Scenes/David Pacheco/Scripts/Movement.cs
@@ -7,12 +7,18 @@ public class Movement : MonoBehaviour {
     public float speed = 20.0f, speedHori = 4.0f;
     public float rotationSpeed = 90f, scale = 0f, rotation;
     public bool ramp = false;
+    public float jumpHeight = 70f;
+    private Rigidbody rb;
+    public float downTime = 0;
+    public float coolDownTime = 2;
+    public bool lavaSpeed = false;
 
-	void Start () {
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 		
 	}
-	
-	void Update () {
+
+    void Update () {
 
         OnButtonPress();
 
@@ -29,6 +35,12 @@ public class Movement : MonoBehaviour {
             rotation *= Time.deltaTime;
             transform.Rotate(0, rotation * translation * (10f / 1.5f), 0);
         }
+        if (Input.GetButton("Jump") && Time.time > downTime)
+        {
+        
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            downTime = Time.time + coolDownTime;
+        }
     }
 
     public void OnTriggerStay(Collider other)
@@ -40,6 +52,14 @@ public class Movement : MonoBehaviour {
 
             scale += 0.05f * Time.deltaTime;
         }
+        if (other.gameObject.tag == "Lava")
+        {
+            Debug.Log("it work?");
+            lavaSpeed = true;
+            speed = 1;
+            jumpHeight = 10;
+
+        }
     }
 
     public void OnTriggerExit(Collider other2)
@@ -49,8 +69,16 @@ public class Movement : MonoBehaviour {
             ramp = false;
             scale = 0;
 
+
+        }
+        if (other2.gameObject.tag == "Lava")
+        {
+            lavaSpeed = false;
         }
     }
+
+
+
 
     public void OnButtonPress()
     {
@@ -62,11 +90,13 @@ public class Movement : MonoBehaviour {
             translation *= Time.deltaTime;
             transform.Translate(-translation , 0, 0);
         }
-        else if (!Input.GetKey(KeyCode.LeftShift))
+        else if (!Input.GetKey(KeyCode.LeftShift)&& lavaSpeed == false)
         {
+
             rotationSpeed = 90f;
             speed = 20.0f;
         }
     }
+
    
 }
