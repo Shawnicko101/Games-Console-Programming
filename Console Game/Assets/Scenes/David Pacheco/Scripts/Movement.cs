ec18c6919197b35f06af919cd11f210395a134b7
PROgizmo <conor.lynch@mail.bcu.ac.uk>
2019-03-08 14:36:33 +0000

    UI, Powers and Powerup

--- Console Game/Assets/Scenes/David Pacheco/Scripts/Movement.cs
+++ Console Game/Assets/Scenes/David Pacheco/Scripts/Movement.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public GameObject Freeze;
     public float speed = 20.0f, speedHori = 4.0f;
     public float rotationSpeed = 90f, scale = 0f, rotation;
     public bool ramp = false;
+    public float power_cooldown = 4.0f;
+    public float last_Power = 0.0f;
 
-	void Start () {
+    void Start () {
 		
 	}
 	
 	void Update () {
-
+        Ability();
         OnButtonPress();
+        
 
         float translation = Input.GetAxis("Vertical") * speed;
 
@@ -67,6 +71,23 @@ public class Movement : MonoBehaviour {
             rotationSpeed = 90f;
             speed = 20.0f;
         }
+
+
     }
-   
+
+    public void Ability()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            if (Time.time - last_Power < power_cooldown)
+            {
+                return;
+            }
+
+            Instantiate(Freeze, gameObject.transform.position + (transform.forward * 2), Quaternion.Euler(90, 0, 0));
+            last_Power = Time.time;
+        }
+    }
+
 }
