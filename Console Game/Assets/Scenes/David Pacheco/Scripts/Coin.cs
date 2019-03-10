a951436dbcd2fcb6108160d606566a69657b0185
DavidOPacheco <DavidOPacheco@users.noreply.github.com>
2019-03-10 22:41:01 +0000

    Updated My level

---
+++ Console Game/Assets/Scenes/David Pacheco/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        transform.Rotate(0, 2f, 0);
+
+    }
+}
