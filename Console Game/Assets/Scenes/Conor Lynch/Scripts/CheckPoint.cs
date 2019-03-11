20fc7bb7e2f47dc935076074e0144a98db1ba3a9
PROgizmo <conor.lynch@mail.bcu.ac.uk>
2019-03-11 14:26:08 +0000

    not working

---
+++ Console Game/Assets/Scenes/Conor Lynch/Scripts/CheckPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+    private Transform PlayerTransform;
+
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.Find("Bike");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (other collider)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        if (transform == PlayerTransform.GetComponent (checkPoint).checkPoint
+        {
+
+        }
+    }
+}
