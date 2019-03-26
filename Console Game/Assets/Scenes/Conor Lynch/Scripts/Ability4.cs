41d901b76155c1d0efd3b6c158a355a7eeb4622f
Conor1997 <47567891+Conor1997@users.noreply.github.com>
2019-03-26 15:30:48 +0000

    Added 4/5 of powers

---
+++ Console Game/Assets/Scenes/Conor Lynch/Scripts/Ability4.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability4 : MonoBehaviour
+{
+    public float Abilityduration = 4f;
+    float timer;
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        timer = Abilityduration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+        transform.position = player.transform.position;
+
+
+    }
+
+}
