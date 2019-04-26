2567c3e523cf0e4c48ba79f08d089f1031a081a3
Conor Lynch <Conor.Lynch@mail.bcu.ac.uk>
2019-04-26 16:04:11 +0100

    Added Character and Car Customisation

---
+++ Console Game/Assets/Scenes/David Pacheco/Scripts/CarSelect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSelect : MonoBehaviour {
+    public Image Character1, Character2, Character3, Character4, Character5;
+    public Text currentPlayer;
+    int CarNumber = 0;
+
+    public int ChosenCar = 0;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        ChangeCar();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (CarNumber <= 3)
+            {
+                CarNumber++;
+            }
+            else if (CarNumber >= 4)
+            {
+                CarNumber = 0;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ChosenCar = CarNumber;
+        }
+    }
+
+    public void ChangeCar()
+    {
+        switch (CarNumber)
+
+        {
+            case 0:
+                Character1.enabled = true;
+                Character2.enabled = false;
+                Character5.enabled = false;
+                break;
+            case 1:
+                Character1.enabled = false;
+                Character2.enabled = true;
+                Character3.enabled = false;
+                break;
+            case 2:
+                Character2.enabled = false;
+                Character3.enabled = true;
+                Character4.enabled = false;
+                break;
+            case 3:
+                Character3.enabled = false;
+                Character4.enabled = true;
+                Character5.enabled = false;
+
+                break;
+            case 4:
+                Character1.enabled = false;
+                Character4.enabled = false;
+                Character5.enabled = true;
+                break;
+        }
+    }
+}
---
+++ Console Game/Assets/Scenes/David Pacheco/Scripts/CharacterSelect.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour {
+    public Image Character1, Character2, Character3, Character4, Character5, Ability1, Ability2, Ability3, Ability4, Ability5;
+    public Text abilityName, currentPlayer;
+    int CharacterNumber = 0;
+
+    public int ChosenCharacter = 0, ChosenCar = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeCharacter();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (CharacterNumber <= 3)
+            {
+                CharacterNumber++;
+            }
+            else if (CharacterNumber >= 4)
+            {
+                CharacterNumber = 0;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ChosenCharacter = CharacterNumber;
+        }
+
+	}
+
+    public void ChangeCharacter()
+    {
+        switch(CharacterNumber)
+
+        {
+            case 0:
+                Character1.enabled = true;
+                Character2.enabled = false;
+                Character5.enabled = false;
+                Ability1.enabled = true;
+                Ability2.enabled = false;
+                Ability5.enabled = false;
+
+                abilityName.text = "Water Ability";
+                break;
+            case 1:
+                Character1.enabled = false;
+                Character2.enabled = true;
+                Character3.enabled = false;
+                Ability1.enabled = false;
+                Ability2.enabled = true;
+                Ability3.enabled = false;
+
+                abilityName.text = "Fire Ability";
+                break;
+            case 2:
+                Character2.enabled = false;
+                Character3.enabled = true;
+                Character4.enabled = false;
+                Ability2.enabled = false;
+                Ability3.enabled = true;
+                Ability4.enabled = false;
+
+                abilityName.text = "Freeze Ability";
+                break;
+            case 3:
+                Character3.enabled = false;
+                Character4.enabled = true;
+                Character5.enabled = false;
+                Ability3.enabled = false;
+                Ability4.enabled = true;
+                Ability5.enabled = false;
+
+                abilityName.text = "Storm Ability";
+
+                break;
+            case 4:
+                Character1.enabled = false;
+                Character4.enabled = false;
+                Character5.enabled = true;
+                Ability4.enabled = false;
+                Ability5.enabled = true;
+                Ability1.enabled = false;
+
+                abilityName.text = "Lightning Ability";
+                break;
+        }
+    }
+}
+
